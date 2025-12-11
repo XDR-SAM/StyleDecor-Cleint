3521e7146998bc28c25c50876d1d2974d4bcc83f
XDR-SAM <tdxfarhan@gmail.com>
2025-12-11 12:46:03 +0600

    loading spinner

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center min-h-[400px]">
+      <span className="loading loading-spinner loading-lg text-accent"></span>
+    </div>
+  );
+};
+
+export default Loading;
+
