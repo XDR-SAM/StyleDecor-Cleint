a9245e6c69b3b4cbf5d2dfe76650adda0da1e540
XDR-SAM <tdxfarhan@gmail.com>
2025-12-11 12:46:53 +0600

    layout context

---
+++ src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 transition-colors duration-300">
+      <Navbar />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
+
